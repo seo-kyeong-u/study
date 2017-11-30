09de0c24d2a42d3e68ac6181fcf5c3c535601be4
sw26 <sw26@sw26-PC>
2017-11-30 20:03:44 +0900

    modify Gugudan java

--- study_project/src/com/test/study/Gugudan.java
+++ study_project/src/com/test/study/Gugudan.java
@@ -10,6 +10,7 @@ public class Gugudan {
 			}
 			System.out.println();
 		}
-		System.out.println("hotfix");                
+		System.out.println("hotfix");
+		System.out.println("hotfix2");
 	}
 }
